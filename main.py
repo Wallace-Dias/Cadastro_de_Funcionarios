11c53d190f5841c7d3924022dc5ccc55c1fbe374
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-19 17:26:45 -0300

    Implementando nova funcionalidade
    Adicionei uma opção onde também é possível apagar toda a listagem de  funcionários ao invés de apagar um por um.

--- main.py
+++ main.py
@@ -34,21 +34,27 @@ while True:
                 print("")
 
                 
+            # Lista de Funcionários
             if opc == 2:
-                # Lista de Funcionários
-                print("="*30)
-                print("Lista de Funcionários".center(30))
-                print("="*30)
-                for index, funcionário in enumerate(funcionários, start=1):
-                    nome, idade, salário = funcionário
-                    print(f"{index}\nNome: {nome}\nIdade: {idade}\nSalário: R$ {salário:.2f}\n")
+                if len(funcionários) == 0:
+                    print("Lista Vazia")
                     print("="*30)
-                
-                print("="*30)
-                print("")
+                    print("")
+
+                else:
+                    
+                    print("Lista de Funcionários".center(30))
+                    print("="*30)
+                    for index, funcionário in enumerate(funcionários, start=1):
+                        nome, idade, salário = funcionário
+                        print(f"{index}\nNome: {nome}\nIdade: {idade}\nSalário: R$ {salário:.2f}\n")
+                        print("="*30)
+                    
+                    print("="*30)
+                    print("")
             
+            # Pesquisar Funcionários
             if opc == 3:
-                # Pesquisar
                 # Em construção . . .
                 print("Pesquisar Funcionário".center(30))
                 print("="*30)
@@ -81,48 +87,56 @@ while True:
                 print("="*30)
                 print("")
             
+            # Deletar 1 funcionário ou todos
             if opc == 4:
+                esq = int(input("[1] - Deletar funcionário em específico\n[2] - Excluir todos os funcionários\nOpcão: "))
 
-                pesq = input("Nome do Funcionário a ser\nexcluído: ")
-                encontrado = False
-                for funcionário in funcionários:
-                    nome, idade, salário = funcionário
+                if esq == 1:
 
-                    if pesq.lower() == nome.lower():
-                        print("="*30)
-                        print(f'\nNome: {nome}\nIdade: {idade}\nSalário: R$ {salário: .2f}\n')
-                        print("="*30)
-                        encontrado = True
+                    pesq = input("Nome do Funcionário a ser\nexcluído: ")
+                    encontrado = False
+                    for funcionário in funcionários:
+                        nome, idade, salário = funcionário
 
-                        delete = input("Deletar? S/N: ")
-                        print("")
-
-                        if delete in 'sn':
-
-                            if delete.lower() == 's':
-                                funcionários.remove(funcionário)
-
-                            if delete.lower() == 'n':
-                                print("Exclusão cancelada")
-                        else:
+                        if pesq.lower() == nome.lower():
                             print("="*30)
-                            print("Opção inválida!".center(30))
+                            print(f'\nNome: {nome}\nIdade: {idade}\nSalário: R$ {salário: .2f}\n')
                             print("="*30)
+                            encontrado = True
+
+                            delete = input("Deletar? S/N: ")
                             print("")
 
-                
-                if not encontrado:
-                    print("="*30)
-                    print("Funcionário não encontrado!")
-                    print("="*30)
+                            if delete in 'sn':
 
+                                if delete.lower() == 's':
+                                    funcionários.remove(funcionário)
 
+                                if delete.lower() == 'n':
+                                    print("Exclusão cancelada")
+                            else:
+                                print("="*30)
+                                print("Opção inválida!".center(30))
+                                print("="*30)
+                                print("")
 
+                    
+                    if not encontrado:
+                        print("="*30)
+                        print("Funcionário não encontrado!")
+                        print("="*30)
 
+                # Deleta todos
+                elif esq == 2:
+                    print("\nApagando todos os cadastros . . .")
+                    sleep(2)
+                    funcionários.clear()
+                    print("\nLista Vazia")
+                else:
+                    print("\nValor Inválido!")
 
-
+            # Sair do programa
             if opc == 5:
-                # Sair do programa
                 print("Saindo . . .")
                 sleep(2)
                 break
--- rascunho.py
+++ rascunho.py
@@ -9,7 +9,13 @@
 
 
 
-# Testes de Código
+# Problemas encontrados
+
+""" 
+Partes onde é pedido um input para o usuário para escolher uma opção podem apresentar erros caso ele digite errado, exemplo:
+- Colocar um número no lugar do nome, o código aceita apesar de ser um erros
+- Colocar uma letra onde deveria ser a idade ou o salário, isso causara erro, sugiro usar uma verificação com o .isdigit()
+ """
 
 
 
